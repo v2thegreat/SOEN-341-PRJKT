20990bf8f0cf96728c163e7862a7a276fe247afb
WanProduction <48234317+WanProduction@users.noreply.github.com>
2019-10-06 16:37:36 -0400

    Add files via upload

--- soen341script.js
+++ soen341script.js
@@ -1,31 +1,12 @@
 <script>
 function openNav() {
-  document.getElementById("channelSidenav").style.width = "250px";
+  document.getElementById("friendSideNav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
 }
 
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("channelSidenav").style.width = "0";
+  document.getElementById("friendSideNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
 }
-
-function openChannel(evt, channelName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the link that opened the tab
-  document.getElementById(channelName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
 </script>
